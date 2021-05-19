59d6a12e0d347787c8463516ffe5a6f314a56acf
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-05-19 20:55:47 -0300

    style
    aparência da calculadora adicionada

--- App.js
+++ App.js
@@ -2,24 +2,100 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useState,useEffect} from 'react';
 import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View, Modal,TouchableHighlight, Button, SafeAreaView,SectionList } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import Constants from 'expo-constants';
 import { Feather,AntDesign } from '@expo/vector-icons';
 
 export default function App() {
 
+  const buttonValues = ['7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+'];
+  const signs = ['*','/','+','-'];
+  const signsAll = ['*','/','+','-','(',')','sqr'];
+  const characters = ['Calcule os parenteses','Calcule as multiplicações','Calcule as Divisões','Calcule as adições','Calcule as subtrações']
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <LinearGradient colors={['rgba(255,255,255,0.3)', 'transparent']} style={{width:'100%',height:'100%',position:'absolute'}}/>
+      <LinearGradient colors={['rgba(255,255,255,0.3)', 'transparent']} style={{...styles.gradient,top:0}}/>  
+      <View style={{width:'100%',height:'100%',position:'relative'}}>
 
-      {/*painel*/}
-      <View style={{width:'100%',height:'75%',padding:'10%',borderColor:'red',borderWidth:1}}>
-        <View style={{width:'100%',height:'100%',borderColor:'red',borderWidth:1}}>
 
+
+        {/*painel*/}
+        <View style={{width:'100%',height:'35%',padding:'7.5%',paddingBottom:0}}>
+          <View style={{width:'100%',height:'100%',borderColor:'black',borderWidth:5,backgroundColor:'rgba(255,255,255,0.75)'}}>
+
+          </View>
         </View>
-      </View>
 
-      {/*controle*/}
-      <View style={{width:'100%',height:'100%',borderColor:'red',borderWidth:1}}>
+
+
+        {/*barra de controle*/}
+        <View style={{width:'94%',height:'8%',flexDirection:'row',alignItems:'center',marginHorizontal:'3%'}}>
+          <TouchableOpacity style={{width:'30%',height:'100%',alignItems:'center',alignContent:'center'}}>
+            <AntDesign name="arrowleft" size={40} color='black' style={{textAlignVertical:'center',textAlign:'center',width:'100%',height:'100%'}} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{width:'40%',height:'75%',backgroundColor:'rgba(255,255,255,0.75)',...styles.btn}}>            
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{width:'30%',height:'100%',alignItems:'center'}}>
+            <AntDesign name="arrowright" size={40} color='black' style={{textAlignVertical:'center',textAlign:'center',width:'100%',height:'100%'}} />
+          </TouchableOpacity>
+        </View>
+
+
+
+        {/*área de botões*/}
+        <View style={{width:'94%',height:'55%',borderColor:'rgba(0,0,0,0.5)',borderWidth:0.5,marginHorizontal:'3%',backgroundColor:'rgba(28, 130, 133,0.3)',borderRadius:5}}>
+          <LinearGradient colors={['rgba(255,255,255,0.4)', 'transparent']} style={styles.gradient}/>
+
+          {/*barra*/}
+          <View style={{width:'100%',height:'14%',flexDirection:'row',padding:'1%',backgroundColor:'rgba(25, 59, 57,0.25)',borderRadius:5,borderBottomWidth:0.5}}>
+
+            <View style={{width:'70%',height:'100%',flexDirection:'row'}}>
+
+              <TouchableOpacity style={{width:'20%',marginVertical:'1.5%',marginLeft:'20%',backgroundColor: 'rgba(182, 227, 225,0.1)',...styles.btn,borderRadius:100}}>
+                <LinearGradient colors={['rgba(255,255,255,0.2)', 'transparent']} style={styles.gradient}/>
+                <Text style={{height:'100%',fontSize:20,textAlign:'center',textAlignVertical:'center'}}>{'( )'}</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{width:'20%',marginVertical:'1.5%',marginLeft:'20%',backgroundColor: 'rgba(182, 227, 225,0.1)',...styles.btn,borderRadius:100}}>
+                <LinearGradient colors={['rgba(255,255,255,0.2)', 'transparent']} style={styles.gradient}/>
+                <Text style={{height:'100%',fontSize:20,textAlign:'center',textAlignVertical:'center'}}>{'√'}</Text>
+              </TouchableOpacity>
+
+            </View>
+
+            <TouchableOpacity style={{width:'30%',height:'100%',backgroundColor:'rgba(255,20,20,0.275)',...styles.btn}}>
+              <LinearGradient colors={['rgba(255,255,255,0.2)', 'transparent']} style={styles.gradient}/>
+              <Feather name="delete" size={28} color="black" style={{height:'100%',textAlign:'center',textAlignVertical:'center'}} />
+            </TouchableOpacity>
+          </View>
+
+          {/*botões*/}
+          <View style={{width:'100%',height:'86%',flexDirection:'row',flexWrap:'wrap',padding:'1%'}}>
+            {buttonValues.map((val)=>{
+              let rad = 100;
+              let fs = 30;
+              let bgCol = 'rgba(182, 227, 225,0.35)';
+              if(signs.includes(val)||val=='='){
+                rad = 10;
+                fs = 40;
+                bgCol = 'rgba(182, 227, 225,0.2)';
+                fs = 30;
+              }
+              else if(val=='.')
+                fs = 30;
+              return(
+                <TouchableOpacity style={{width:'22%',height:'22%',margin:'1.5%',backgroundColor: bgCol,...styles.btn,borderRadius:rad}}>
+                  <Text style={{height:'100%',fontSize:fs,textAlign:'center',textAlignVertical:'center'}}>{val=='/'?'÷':val=='*'?'x':val}</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+
+
 
       </View>
     </View>
@@ -27,11 +103,22 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     backgroundColor: 'rgb(43, 105, 91)',
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop:20
+    justifyContent: 'center'
   },
+  btn:{
+    borderWidth:0.5,
+    borderTopWidth:0.25,
+    borderBottomWidth:1,
+    borderRadius:5,
+    overflow:'hidden'
+  },
+  gradient:{
+    width:'100%',
+    height:'100%',
+    position:'absolute'
+  }
 });
