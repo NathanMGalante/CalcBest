b8a2794a5a63f6c911b8e7ad1da360d3ba7569f7
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-05-19 17:57:40 -0300

    install conponents

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View, Modal,TouchableHighlight, Button, SafeAreaView,SectionList } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Feather,AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <LinearGradient colors={['rgba(255,255,255,0.3)', 'transparent']} style={{width:'100%',height:'100%',position:'absolute'}}/>
+
+      {/*painel*/}
+      <View style={{width:'100%',height:'75%',padding:'10%',borderColor:'red',borderWidth:1}}>
+        <View style={{width:'100%',height:'100%',borderColor:'red',borderWidth:1}}>
+
+        </View>
+      </View>
+
+      {/*controle*/}
+      <View style={{width:'100%',height:'100%',borderColor:'red',borderWidth:1}}>
+
+      </View>
     </View>
   );
 }
@@ -14,8 +29,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(43, 105, 91)',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:20
   },
 });
